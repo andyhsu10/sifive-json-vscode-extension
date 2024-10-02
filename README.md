250c3d9677e6d7f959435d2a119f3e9bdd0c0e30
An-Ting Hsu <andyhsu10@gmail.com>
2024-10-02 16:49:04 +0800

    chore: rename package name

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sifive-json-vscode-extension README
 
-This is the README for your extension "sifive-json-validator". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "sifive-json-vscode-extension". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--- src/extension.ts
+++ src/extension.ts
@@ -8,19 +8,19 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
-    'Congratulations, your extension "sifive-json-validator" is now active!',
+    'Congratulations, your extension "sifive-json-vscode-extension" is now active!',
   );
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    'sifive-json-validator.helloWorld',
+    'sifive-json-vscode-extension.helloWorld',
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
       vscode.window.showInformationMessage(
-        'Hello World from sifive-json-validator!',
+        'Hello World from sifive-json-vscode-extension!',
       );
     },
   );
